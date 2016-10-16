ae36ccb04b8d34febd942c27d08c307ddb4c064e
Vishal Sanghai <vcoolsanghai@gmail.com>
2016-10-16 17:59:20 +0530

    Morse code added to terminal page

---
+++ vishal/js/morse.js
@@ -0,0 +1,80 @@
+function bluechange(){
+    $(".bluecircle").css("background","transparent");
+    console.log("HI");
+}
+function bluebackchange(){
+    $(".bluecircle").css("background","#589FD6");
+    console.log("HI2");
+}
+function greenchange(){
+    $(".greencircle").css("background","transparent");
+    console.log("HI");
+}
+function greenbackchange(){
+    $(".greencircle").css("background","#26A69A");
+    console.log("HI2");
+}
+function redchange(){
+    $(".redcircle").css("background","transparent");
+    console.log("HI");
+}
+function redbackchange(){
+    $(".redcircle").css("background","#EE5451");
+    console.log("HI2");
+}
+function purplechange(){
+    $(".purplecircle").css("background","transparent");
+    console.log("HI");
+}
+function purplebackchange(){
+    $(".purplecircle").css("background","#775BA7");
+    console.log("HI2");
+}
+function yellowchange(){
+    $(".yellowcircle").css("background","transparent");
+    console.log("HI");
+}
+function yellowbackchange(){
+    $(".yellowcircle").css("background","#FDED62");
+    console.log("HI2");
+}
+setTimeout(bluechange,1000);
+setTimeout(bluebackchange,1500);
+setTimeout(bluechange,3500);
+setTimeout(bluebackchange,4000);
+setTimeout(bluechange,6000);
+setTimeout(bluebackchange,6500);
+
+setTimeout(greenchange,7000);
+setTimeout(greenbackchange,7500);
+setTimeout(greenchange,9500);
+setTimeout(greenbackchange,10000);
+setTimeout(greenchange,12000);
+setTimeout(greenbackchange,12500);
+
+setTimeout(redchange,13000);
+setTimeout(redbackchange,13500);
+setTimeout(redchange,15500);
+setTimeout(redbackchange,16000);
+setTimeout(redchange,18000);
+setTimeout(redbackchange,18500);
+
+setTimeout(purplechange,19000);
+setTimeout(purplebackchange,19500);
+setTimeout(purplechange,21500);
+setTimeout(purplebackchange,22000);
+setTimeout(purplechange,24000);
+setTimeout(purplebackchange,24500);
+
+setTimeout(yellowchange,25000);
+setTimeout(yellowbackchange,25500);
+setTimeout(yellowchange,27500);
+setTimeout(yellowbackchange,28000);
+setTimeout(yellowchange,30000);
+setTimeout(yellowbackchange,30500);
+
+
+//$(".bluecircle").css("background","#589FD6");
+//setTimeout(change,2000);
+
+
